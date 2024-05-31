cf7abf195ce2506ac05cc5d01ae0966f69bf6ba3
Julieta Toledo <Julieta Toledo@LAPTOP-MK61DTNB>
2024-05-31 19:59:33 -0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace pryCalculadorPromedios
+{
+    partial class fmrCalculadorPromedio
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grpDatos = new System.Windows.Forms.GroupBox();
+            this.txtNota2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.txtNota1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtAlumno = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnPromedioClase = new System.Windows.Forms.Button();
+            this.grpResultado = new System.Windows.Forms.GroupBox();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.grpDatos.SuspendLayout();
+            this.grpResultado.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grpDatos
+            // 
+            this.grpDatos.Controls.Add(this.txtNota2);
+            this.grpDatos.Controls.Add(this.label3);
+            this.grpDatos.Controls.Add(this.btnCalcular);
+            this.grpDatos.Controls.Add(this.txtNota1);
+            this.grpDatos.Controls.Add(this.label2);
+            this.grpDatos.Controls.Add(this.txtAlumno);
+            this.grpDatos.Controls.Add(this.label1);
+            this.grpDatos.Location = new System.Drawing.Point(52, 49);
+            this.grpDatos.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.grpDatos.Name = "grpDatos";
+            this.grpDatos.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.grpDatos.Size = new System.Drawing.Size(515, 277);
+            this.grpDatos.TabIndex = 0;
+            this.grpDatos.TabStop = false;
+            this.grpDatos.Text = "Datos";
+            // 
+            // txtNota2
+            // 
+            this.txtNota2.Location = new System.Drawing.Point(145, 160);
+            this.txtNota2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtNota2.Name = "txtNota2";
+            this.txtNota2.Size = new System.Drawing.Size(275, 22);
+            this.txtNota2.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(83, 164);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Nota 2";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(197, 204);
+            this.btnCalcular.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(128, 41);
+            this.btnCalcular.TabIndex = 4;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // txtNota1
+            // 
+            this.txtNota1.Location = new System.Drawing.Point(145, 105);
+            this.txtNota1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtNota1.Name = "txtNota1";
+            this.txtNota1.Size = new System.Drawing.Size(275, 22);
+            this.txtNota1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(83, 108);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nota 1";
+            // 
+            // txtAlumno
+            // 
+            this.txtAlumno.Location = new System.Drawing.Point(145, 50);
+            this.txtAlumno.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtAlumno.Name = "txtAlumno";
+            this.txtAlumno.Size = new System.Drawing.Size(275, 22);
+            this.txtAlumno.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(83, 59);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Alumno";
+            // 
+            // btnPromedioClase
+            // 
+            this.btnPromedioClase.Location = new System.Drawing.Point(380, 364);
+            this.btnPromedioClase.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnPromedioClase.Name = "btnPromedioClase";
+            this.btnPromedioClase.Size = new System.Drawing.Size(187, 41);
+            this.btnPromedioClase.TabIndex = 5;
+            this.btnPromedioClase.Text = "Calcular promedio clase";
+            this.btnPromedioClase.UseVisualStyleBackColor = true;
+            this.btnPromedioClase.Click += new System.EventHandler(this.btnPromedioClase_Click);
+            // 
+            // grpResultado
+            // 
+            this.grpResultado.Controls.Add(this.lblResultado);
+            this.grpResultado.Location = new System.Drawing.Point(53, 352);
+            this.grpResultado.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.grpResultado.Name = "grpResultado";
+            this.grpResultado.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.grpResultado.Size = new System.Drawing.Size(319, 166);
+            this.grpResultado.TabIndex = 6;
+            this.grpResultado.TabStop = false;
+            this.grpResultado.Text = "Resultado";
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.Location = new System.Drawing.Point(21, 36);
+            this.lblResultado.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(277, 108);
+            this.lblResultado.TabIndex = 0;
+            this.lblResultado.Click += new System.EventHandler(this.lblResultado_Click);
+            // 
+            // fmrCalculadorPromedio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(629, 554);
+            this.Controls.Add(this.btnPromedioClase);
+            this.Controls.Add(this.grpResultado);
+            this.Controls.Add(this.grpDatos);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "fmrCalculadorPromedio";
+            this.Text = "Calculador promedio";
+            this.grpDatos.ResumeLayout(false);
+            this.grpDatos.PerformLayout();
+            this.grpResultado.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grpDatos;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.TextBox txtNota1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtAlumno;
+        private System.Windows.Forms.Button btnPromedioClase;
+        private System.Windows.Forms.GroupBox grpResultado;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.TextBox txtNota2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryCalculadorPromedios
+{
+    public partial class fmrCalculadorPromedio : Form
+    {
+        public fmrCalculadorPromedio()
+        {
+            InitializeComponent();
+        }
+        private List<string> alumnos = new List<string>();
+        private List<double> promedios = new List<double>();
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            string nombre = txtAlumno.Text;
+            int numero1 = Convert.ToInt32(txtNota1.Text);
+            int numero2 = Convert.ToInt32(txtNota2.Text);
+
+      
+
+            if( txtAlumno.Text != "" && txtNota1.Text != "" && txtNota2.Text != "")
+            {
+                double promedio = (numero1 + numero2) / 2.0;
+                lblResultado.Text = "El promedio de " + nombre +" es " + promedio.ToString();
+
+                alumnos.Add(nombre);
+                promedios.Add(promedio);
+
+            }
+            else
+            {
+                MessageBox.Show("Por favor, ingrese notas válidas.");
+            }
+
+
+        }
+
+        private void lblResultado_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPromedioClase_Click(object sender, EventArgs e)
+        {
+
+            if (promedios.Count > 0)
+            {
+                
+                double promedioGeneral = 0;
+                foreach (double promedio in promedios)
+                {
+                    promedioGeneral += promedio;
+                }
+                promedioGeneral /= promedios.Count;
+
+                
+               lblResultado.Text = "Promedio general de todos los alumnos: " + promedioGeneral;
+            }
+            else
+            {
+                MessageBox.Show("No hay promedios registrados.");
+            }
+
+        }
+    }
+}
